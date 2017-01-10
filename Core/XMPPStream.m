ab3f23d074d9f0a537d62a1ff3092e415b3e0f1a
Omar <silen08620@gmail.com>
2017-01-10 10:26:23 +0100

    - Bug solve iPv6

--- Core/XMPPStream.m
+++ Core/XMPPStream.m
@@ -210,6 +210,7 @@ enum XMPPStreamConfig
 		
 		// Initialize socket
 		asyncSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:xmppQueue];
+        asyncSocket.IPv4PreferredOverIPv6 = NO;
 	}
 	return self;
 }
